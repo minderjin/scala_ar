59b7b429fede5dfc4fa444023875082bd0b8432e
minerjin <jiniusk@gmail.com>
2017-07-12 14:35:16 +0900

    - AssocitaionRules object 추가

---
+++ src/main/scala/AssociationRules.scala
@@ -0,0 +1,40 @@
+/**
+  * Created by lenovo on 2017-07-12.
+  */
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.fpm.AssociationRules
+import org.apache.spark.mllib.fpm.FPGrowth.FreqItemset
+import org.apache.spark.mllib.fpm.FPGrowth
+
+object AssociationRules {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("AssociationRulesExample")
+    val sc = new SparkContext(conf)
+
+    val data = sc.textFile("./ar.basket")
+
+    val transactions: RDD[Array[String]] = data.map(s => s.trim.split(','))
+
+    val fpg = new FPGrowth()
+      .setMinSupport(0.2)
+      .setNumPartitions(10)
+
+    val model = fpg.run(transactions) // creates the FPGrowthModel
+
+    val ar = new AssociationRules()
+      .setMinConfidence(0.8)
+
+    val results = ar.run(model.freqItemsets)
+
+
+    println("count : " + results.count())
+
+    results.collect().foreach { rule =>
+      println("[" + rule.antecedent.mkString(",")
+        + "=>"
+        + rule.consequent.mkString(",") + "]," + rule.confidence)
+    }
+
+  }
+}
